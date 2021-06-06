6bf0156f9107aa3c9fa42328545b5d183a858932
“Pete <peteskelly@gmail.com>
2021-06-06 15:40:29 -0400

    Combined table and queue output

---
+++ src/CandidateManager.Functions/CandidateProcessingEntity.cs
@@ -0,0 +1,24 @@
+namespace CandidateManager.Functions
+{
+    public class CandidateProcessingEntity
+    {
+        public CandidateProcessingEntity()
+        {
+        }
+
+        public CandidateProcessingEntity(string partitionKey, string rowKey, string flowId, string company, string recruiter, string fileName)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+            this.Company = company;
+            this.Recruiter = recruiter;
+            this.FileName = fileName;
+
+        }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Company { get; set; }
+        public string Recruiter { get; set; }
+        public string FileName { get; set; }
+    }
+}
